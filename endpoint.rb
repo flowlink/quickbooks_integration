10bea0e888558ba566bb1c4ed39240050282ec46
Brian D. Quinn <briandquinn@gmail.com>
2013-02-26 08:16:54 -0500

    Add auth example

--- endpoint.rb
+++ endpoint.rb
@@ -4,8 +4,13 @@ require "sinatra/json"
 class AuguryEndpoint < Sinatra::Base
   helpers Sinatra::JSON
 
+  before do
+    unless request.env["HTTP_X_AUGURY_TOKEN"] == 'x123'
+      halt 401
+    end
+  end
+
   post '/' do
-    puts params
     json "message_id" => params['message_id'],
          "result" => "ok",
          "details" => {
