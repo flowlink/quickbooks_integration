da52852eb49d7ff2eb1c1b29c38a1cd25fccfe69
Peter Berkenbosch <peter@pero-ict.nl>
2013-10-27 08:34:02 +0100

    cleanup some and make sure we check on the presence of credit_cards items as well.

--- quickbooks_endpoint.rb
+++ quickbooks_endpoint.rb
@@ -1,69 +1,14 @@
 require "sinatra/base"
 require "sinatra/json"
 require "endpoint_base"
-require "./lib/client.rb"
 Dir['./lib/**/*.rb'].each { |f| require f }
 
 class QuickbooksEndpoint < EndpointBase
   helpers Sinatra::JSON
 
   post '/persist' do
-
-    begin
-      client = Quickbooks::Base.client(@message[:payload], @message[:message_id], @config)
-      process_result *client.persist
-    rescue Exception => e
-      process_result 500, {
-          'message_id' => @message[:message_id],
-          'notifications' => [
-            {
-              "level" => "error",
-              "subject" => e.message,
-              "description" => e.backtrace.inspect
-            }
-          ]
-        }
-    end
-
-
+    client = Quickbooks::Base.client(@message[:payload], @message[:message_id], @config)
+    process_result *client.persist
   end
 
-  # post '/import' do
-  #   order_import = OrderImporter.new(@message[:payload], @message[:message_id], @config)
-  #   begin
-  #     result = order_import.consume
-  #     code = result.delete("code") || 200
-  #   rescue Exception => e
-  #     code = 500
-  #     result = {"error" => e.message, "backtrace" => e.backtrace.inspect}
-  #   end
-  #   process_result code, result
-  # end
-
-  # post '/update' do
-  #   order_update = OrderUpdater.new(@message[:payload], @message[:message_id], @config)
-  #   begin
-  #     result = order_update.consume
-  #     code = 200
-  #   rescue Exception => e
-  #     code = 500
-  #     result = {"error" => e.message}
-  #   end
-  #   process_result code, result
-
-  # end
-
-  # post '/status/:id_domain/:id' do
-  #   order_status = StatusChecker.new(@message[:payload], @message[:message_id], @config)
-  #   order_status.id = params[:id]
-  #   order_status.idDomain = params[:id_domain]
-  #   begin
-  #     result = order_status.consume
-  #     code = 200
-  #   rescue Exception => e
-  #     code = 500
-  #     result = {"error" => e.message}
-  #   end
-  #   process_result code, result
-  # end
 end
