879c5121240af977eb7db579aca2ee14f9137310
Peter Berkenbosch <peter@pero-ict.nl>
2013-10-28 11:51:04 -0400

    add spec for Sinatra 'perist' for online order:new

--- spec/quickbooks_endpoint_spec.rb
+++ spec/quickbooks_endpoint_spec.rb
@@ -11,31 +11,33 @@ describe QuickbooksEndpoint do
   end
 
 
-  context "windows" do
-    context "persist" do
-    end
-  end
-
-  context "online" do
-
-    let(:message) {
-      {
-        "message" => "order:new",
-        :payload => {
-          "order" => Factories.order,
-          "original" => Factories.original,
-          "parameters" => Factories.parameters
+  context "persist" do
+    context "with order:new" do
+      context "online" do
+        let(:message) {
+          {
+            "message" => "order:new",
+            :message_id => "abc",
+            :payload => {
+              "order" => Factories.order,
+              "original" => Factories.original,
+              "parameters" => Factories.parameters
+            }
+          }
         }
-      }
-    }
 
-    context "persist" do
+        it "should respond to POST 'persist'" do
+          VCR.use_cassette('online/persist_new_order') do
+            post '/persist', message.to_json, auth
+            last_response.status.should eql 200
+            response = JSON.parse(last_response.body)
 
-      it "should respond to POST 'persist'" do
-        pending "make proper test"
-        post '/persist', message.to_json, auth
+            response["message_id"].should eql "abc"
+            response["notifications"].first["subject"].should eql "persisted order R181807170 in Quickbooks"
+            response["notifications"].first["description"].should eql "Quickbooks SalesReceipt id = 36 and idDomain = QBO"
+          end
+        end
       end
-
     end
   end
 
