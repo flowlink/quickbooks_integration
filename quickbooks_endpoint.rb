5f760c7085ef7046b32737f163ce6756dd7f46e9
Peter Berkenbosch <peter@pero-ict.nl>
2013-10-30 17:05:53 -0400

    prepare for the order:updated message

--- quickbooks_endpoint.rb
+++ quickbooks_endpoint.rb
@@ -11,16 +11,31 @@ class QuickbooksEndpoint < EndpointBase
       client = Quickbooks::Base.client(@message[:payload], @message[:message_id], @config, @message[:message])
       result = client.persist
       order_number = @message[:payload]["order"]["number"]
-      process_result 200, {
-        'message_id' => @message[:message_id],
-        'notifications' => [
-          {
-            "level" => "info",
-            "subject" => "Created Quickbooks sales receipt #{result[:id]} for order #{order_number}",
-            "description" => "Quickbooks SalesReceipt id = #{result[:id]} and idDomain = #{result[:id_domain]}"
-          }
-        ]
-      }
+
+      case @message[:message]
+      when "order:new"
+        process_result 200, {
+          'message_id' => @message[:message_id],
+          'notifications' => [
+            {
+              "level" => "info",
+              "subject" => "Created Quickbooks sales receipt #{result[:id]} for order #{order_number}",
+              "description" => "Quickbooks SalesReceipt id = #{result[:id]} and idDomain = #{result[:id_domain]}"
+            }
+          ]
+        }
+      when "order:updated"
+        process_result 200, {
+          'message_id' => @message[:message_id],
+          'notifications' => [
+            {
+              "level" => "info",
+              "subject" => "Updated the Quickbooks sales receipt #{result[:id]} for order #{order_number}",
+              "description" => ""
+            }
+          ]
+        }
+      end
     rescue Exception => exception
       process_result 500, {
         'message_id' => @message_id,
