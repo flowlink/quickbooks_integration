489f0336c8bbbb7cc9f5ecb86ddcced8ceb56c23
Peter Berkenbosch <peter@pero-ict.nl>
2013-11-15 11:49:12 +0100

    remove the windows/desktop parts for now

--- lib/quickbooks/helper.rb
+++ lib/quickbooks/helper.rb
@@ -4,11 +4,6 @@ require 'oauth'
 module Quickbooks
   module Helper
 
-    # Not supported on QBO!
-    def self.ship_method_names service
-      fetch_names_hash service
-    end
-
     def self.payment_method_names service
       fetch_names_hash service
     end
--- spec/quickbooks_endpoint_spec.rb
+++ spec/quickbooks_endpoint_spec.rb
@@ -10,62 +10,56 @@ describe QuickbooksEndpoint do
     described_class
   end
 
-  context "persist" do
+  describe "persist" do
     context "with order:new" do
-      context "online" do
-        let(:message) {
-          {
-            "message" => "order:new",
-            :message_id => "abc",
-            :payload => {
-              "order" => Factories.order,
-              "original" => Factories.original,
-              "parameters" => Factories.parameters
-            }
+      let(:message) {
+        {
+          "message" => "order:new",
+          :message_id => "abc",
+          :payload => {
+            "order" => Factories.order,
+            "original" => Factories.original,
+            "parameters" => Factories.parameters
           }
         }
-        it "generates a json response with an info notification" do
-          CrossReference.any_instance.stub(:lookup).with("R181807170").and_return(nil)
-          VCR.use_cassette('online/persist_new_order') do
-            post '/persist', message.to_json, auth
-            last_response.status.should eql 200
-            response = JSON.parse(last_response.body)
+      }
+      it "generates a json response with an info notification" do
+        CrossReference.any_instance.stub(:lookup).with("R181807170").and_return(nil)
+        VCR.use_cassette('online/persist_new_order') do
+          post '/persist', message.to_json, auth
+          last_response.status.should eql 200
+          response = JSON.parse(last_response.body)
 
-            response["message_id"].should eql "abc"
-            response["notifications"].first["subject"].should eql "Created Quickbooks sales receipt 45 for order R181807170"
-            response["notifications"].first["description"].should eql "Quickbooks SalesReceipt id = 45 and idDomain = QBO"
-          end
+          response["message_id"].should eql "abc"
+          response["notifications"].first["subject"].should eql "Created Quickbooks sales receipt 45 for order R181807170"
+          response["notifications"].first["description"].should eql "Quickbooks SalesReceipt id = 45 and idDomain = QBO"
         end
-
       end
     end
 
     context "with order:updated" do
-      context "online" do
-        let(:message) {
-          {
-            "message" => "order:updated",
-            :message_id => 'abc',
-            :payload => {
-              "order" => Factories.order(Factories.order_changes),
-              "original" => Factories.original,
-              "parameters" => Factories.parameters
-            }
+      let(:message) {
+        {
+          "message" => "order:updated",
+          :message_id => 'abc',
+          :payload => {
+            "order" => Factories.order(Factories.order_changes),
+            "original" => Factories.original,
+            "parameters" => Factories.parameters
           }
         }
+      }
 
-        it "generates a json response with the update info notification" do
-          VCR.use_cassette('online/persist_updated_order') do
-            post '/persist', message.to_json, auth
-            last_response.status.should eql 200
-            response = JSON.parse(last_response.body)
-            response["message_id"].should eql "abc"
-            response["notifications"].first["subject"].should eql "Updated the Quickbooks sales receipt 45 for order R181807170"
-            response["notifications"].first["description"].should eql "Quickbooks SalesReceipt id = 45 and idDomain = QBO"
-          end
+      it "generates a json response with the update info notification" do
+        VCR.use_cassette('online/persist_updated_order') do
+          post '/persist', message.to_json, auth
+          last_response.status.should eql 200
+          response = JSON.parse(last_response.body)
+          response["message_id"].should eql "abc"
+          response["notifications"].first["subject"].should eql "Updated the Quickbooks sales receipt 45 for order R181807170"
+          response["notifications"].first["description"].should eql "Quickbooks SalesReceipt id = 45 and idDomain = QBO"
         end
       end
     end
   end
-
 end
