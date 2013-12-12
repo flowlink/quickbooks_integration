f88c41edbdaa04e5c104e7610d9876b53cfcff15
Washington Luiz <huoxito@gmail.com>
2013-12-12 13:17:56 -0300

    Adapt to new Service::Base api

--- lib/qb_integration.rb
+++ lib/qb_integration.rb
@@ -6,9 +6,9 @@ require 'tzinfo'
 require "adjustment"
 require "cross_reference"
 
+require "qb_integration/helper"
 require "qb_integration/auth"
 require "qb_integration/base"
-require "qb_integration/helper"
 require "qb_integration/online/client"
 require "qb_integration/product_importer"
 require "qb_integration/services/base"
@@ -17,4 +17,5 @@ require "qb_integration/services/item_service"
 
 require "qb_integration/sales_receipt"
 require "qb_integration/address"
-require "qb_integration/payment_method"
+require "qb_integration/services/payment_method"
+require "qb_integration/services/sales_receipt"
---
+++ lib/qb_integration/services/payment_method.rb
@@ -0,0 +1,34 @@
+module QBIntegration
+  module Service
+    class PaymentMethod < Base
+      attr_reader :order, :original
+
+      def initialize(config, payload)
+        super("PaymentMethod", config)
+
+        @order = payload[:order]
+        @original = payload[:original]
+      end
+
+      def augury_name
+        if original.has_key?("credit_cards") && !original["credit_cards"].empty?
+          original["credit_cards"].first["cc_type"]
+        elsif order["payments"]
+          order["payments"].first["payment_method"]
+        else
+          "None"
+        end
+      end
+
+      def qb_name
+        payment_method_name_mapping = config.fetch("quickbooks.payment_method_name")
+        lookup_value!(payment_method_name_mapping.first, augury_name)
+      end
+
+      def matching_payment
+        quickbooks.fetch_by_name(qb_name) ||
+          (raise Exception.new("No PaymentMethod '#{qb_name}' defined in Quickbooks"))
+      end
+    end
+  end
+end
