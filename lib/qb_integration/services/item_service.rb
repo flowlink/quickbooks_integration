83f4bfb0c6a6ae58b5cc2e8fc34cfbceb2dd8707
Washington Luiz <huoxito@gmail.com>
2013-12-12 23:06:12 -0300

    Add Service::Item#find_or_create_by_sku

--- lib/qb_integration/services/item_service.rb
+++ lib/qb_integration/services/item_service.rb
@@ -9,6 +9,18 @@ module QBIntegration
         response = @quickbooks.query("select * from Item where Name = '#{sku}'")
         response.entries.first
       end
+
+      # NOTE what if a product is given?
+      def find_or_create_by_sku(line_item, account = nil)
+        params = {
+          name: line_item[:sku],
+          description: line_item[:name],
+          unit_price: line_item[:price],
+          income_account_ref: account ? account.id : nil
+        }
+
+        find_by_sku(line_item[:sku]) || create(params)
+      end
     end
   end
 end
