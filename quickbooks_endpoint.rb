45afb8b0934c5a0a55862c8d793a68d17c4c7a07
Washington Luiz <huoxito@gmail.com>
2013-12-17 10:29:53 -0300

    Dont care about message name on order:update
    Since we should only sync paid orders it's ok to create a new sales
    receipt on order:update. The order might not be paid when it's first
    completed

--- quickbooks_endpoint.rb
+++ quickbooks_endpoint.rb
@@ -17,31 +17,6 @@ class QuickbooksEndpoint < EndpointBase
     begin
       code, notification = QBIntegration::OrderImporter.new(@message, @config).sync
       process_result code, notification
-
-      # case @message[:message]
-      # when "order:new"
-      #   process_result 200, {
-      #     'message_id' => @message[:message_id],
-      #     'notifications' => [
-      #       {
-      #         "level" => "info",
-      #         "subject" => "Created Quickbooks sales receipt #{sales_receipt.id} for order #{sales_receipt.doc_number}",
-      #         "description" => "Quickbooks SalesReceipt id = #{result["xref"][:id]}"
-      #       }
-      #     ]
-      #   }
-      # when "order:updated"
-      #   process_result 200, {
-      #     'message_id' => @message[:message_id],
-      #     'notifications' => [
-      #       {
-      #         "level" => "info",
-      #         "subject" => "Updated the Quickbooks sales receipt #{result["xref"][:id]} for order #{order_number}",
-      #         "description" => "Quickbooks SalesReceipt id = #{result["xref"][:id]} and idDomain = #{result["xref"][:id_domain]}"
-      #       }
-      #     ]
-      #   }
-      # end
     rescue Exception => exception
       process_result 500, {
         'message_id' => @message_id,
