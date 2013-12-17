ad2102d42f9ebd470a3311b6ff0c066ef2d3c216
Bruno Buccolo <bruno.buccolo@gmail.com>
2013-12-17 12:11:52 -0200

    Removes duplicate require

--- lib/qb_integration.rb
+++ lib/qb_integration.rb
@@ -3,20 +3,18 @@ $:.unshift File.dirname(__FILE__)
 require 'oauth'
 require 'quickbooks-ruby'
 
-require "qb_integration/helper"
-require "qb_integration/auth"
-require "qb_integration/base"
-require "qb_integration/product_importer"
-require "qb_integration/order_importer"
-require "qb_integration/services/base"
-require "qb_integration/services/account_service"
-require "qb_integration/services/item_service"
+require 'qb_integration/helper'
+require 'qb_integration/auth'
+require 'qb_integration/base'
+require 'qb_integration/product_importer'
+require 'qb_integration/order_importer'
+require 'qb_integration/services/base'
+require 'qb_integration/services/account_service'
+require 'qb_integration/services/item_service'
 
-require "qb_integration/order_importer"
-require "qb_integration/address"
-require "qb_integration/services/payment_method"
-require "qb_integration/services/customer"
-require "qb_integration/services/line"
-require "qb_integration/services/sales_receipt"
-
-require "quickbooks-ruby"
+require 'qb_integration/order_importer'
+require 'qb_integration/address'
+require 'qb_integration/services/payment_method'
+require 'qb_integration/services/customer'
+require 'qb_integration/services/line'
+require 'qb_integration/services/sales_receipt'
