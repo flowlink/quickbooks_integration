6600c3dbd7178c8f8ba1ec2211d221fc495f488d
Washington Luiz <huoxito@gmail.com>
2014-03-21 02:52:37 -0300

    Drop deprecated message stuff in ReturnAuthorization

--- lib/qb_integration/return_authorization.rb
+++ lib/qb_integration/return_authorization.rb
@@ -40,45 +40,8 @@ module QBIntegration
       end
     end
 
-    def sync
-      if sales_receipt = sales_receipt_service.find_by_order_number
-        case message_name
-        when "return_authorization:new"
-          credit_memo = credit_memo_service.create_from_return ra, sales_receipt
-          text = "Created Quickbooks Credit Memo #{credit_memo.id} for return #{ra[:number]}"
-          [200, notification(text)]
-        when "return_authorization:updated"
-          if credit_memo = credit_memo_service.find_by_number(ra[:number])
-            credit_memo_service.update credit_memo, ra, sales_receipt
-            text = "Updated Quickbooks Credit Memo #{credit_memo.id} for return #{ra[:number]}"
-            [200, notification(text)]
-          else
-            credit_memo = credit_memo_service.create_from_return ra, sales_receipt
-            text = "Created Quickbooks Credit Memo #{credit_memo.id} for return #{ra[:number]}"
-            [200, notification(text)]
-          end
-        else
-          [500, notification("Cand handle message #{message_name}", "error")]
-        end
-      else
-        [500, notification("Received return for order not sync with quickbooks", "error")]
-      end
-    end
-
     def sales_receipt_service
       Service::SalesReceipt.new(config, payload, dependencies: false)
-    end
-
-    def notification(text, level = 'info')
-      { 'message_id' => message_id,
-        'notifications' => [
-          {
-            'level' => level,
-            'subject' => text,
-            'description' => text
-          }
-        ]
-      }.with_indifferent_access
     end
   end
 end
