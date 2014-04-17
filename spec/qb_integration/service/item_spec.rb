c27dd705823506d26b16aa0361a665f7952d6770
Washington Luiz <huoxito@gmail.com>
2014-04-17 16:11:18 -0300

    Replay item_spec cassetes

--- spec/qb_integration/service/item_spec.rb
+++ spec/qb_integration/service/item_spec.rb
@@ -25,49 +25,53 @@ describe QBIntegration::Service::Item do
   end
 
   it "creates an item with given attributes" do
+    new_sku = "given"
     VCR.use_cassette("item/create", match_requests_on: [:method, :body]) do
       item = subject.create({
-        name: "NEW-SKU-HERE",
+        name: new_sku,
         description: "Something witty.",
         unit_price: 99.90
       })
 
       expect(item.id).to be
-      expect(item.active).to be
-      expect(item.name).to eq 'NEW-SKU-HERE'
+      expect(item).to be_active
+      expect(item.name).to eq new_sku
       expect(item.unit_price.to_f).to eq 99.90
     end
   end
 
   it "updates an item with given attributes" do
+    new_sku = "given"
     VCR.use_cassette "item/update", match_requests_on: [:method, :body] do
-      item_to_update = subject.find_by_sku('NEW-SKU-HERE')
+      item_to_update = subject.find_by_sku new_sku
       item = subject.update(item_to_update, { description: "new description" })
 
       expect(item.id).to eq item_to_update.id
-      expect(item.active).to be
+      expect(item).to be_active
       expect(item.description).to eq "new description"
     end
   end
 
   context ".find_or_create_by_sku" do
-    let(:line_item) { Factories.order["line_items"].last.with_indifferent_access }
+    let(:line_item) do
+      { "quantity"=> 3, "price"=> 19.99 }.with_indifferent_access
+    end
 
     it "creates new item when it's not there already" do
-      line_item[:sku] = 'T-SHIRT-PUGS-RULE'
+      line_item[:product_id] = 'T-SHIRT-PUGS-RUUUUULEE'
 
       VCR.use_cassette "item/find_or_create", match_requests_on: [:method, :body] do
         subject.stub find_by_sku: nil
         item = subject.find_or_create_by_sku line_item
-        expect(item.name).to eq line_item[:sku]
+        expect(item.name).to eq line_item[:product_id]
       end
     end
 
     it "returns existing item" do
       VCR.use_cassette("item/find_by_sku", match_requests_on: [:method, :body]) do
-        line_item[:sku] = 'T-SHIRT-PUGS-RULE'
+        line_item[:product_id] = 'T-SHIRT-PUGS-RULE'
         item = subject.find_or_create_by_sku line_item
-        expect(item.name).to eq line_item[:sku]
+        expect(item.name).to eq line_item[:product_id]
       end
     end
   end
