23ad73ab46c68b5480353e04bd4031d128fa97fe
Washington Luiz <huoxito@gmail.com>
2014-04-23 18:33:47 -0300

    Proper escaping quote fix (botsy insult washington)
    Still need to review other places on code base where we query using user
    input

---
+++ spec/qb_integration/order_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+module QBIntegration
+  describe Order do
+    subject do
+      described_class.new(message, config)
+    end
+
+    let(:message) do
+      { order: Factories.add_order }
+    end
+
+    let(:config) { Factories.config }
+
+    context "quotes on customer name" do
+      it "handles it just fine" do
+        VCR.use_cassette("sales_receipt/quotes_involved", match_requests_on: [:body, :method]) do
+          subject.create
+        end
+      end
+    end
+  end
+end
