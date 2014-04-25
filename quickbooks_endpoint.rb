36af6fe66308ecf9c5dfaae8ad0d6ff63bd3203f
Brian Quinn <briandquinn@gmail.com>
2014-04-25 12:38:40 +0100

    Included Honeybadger stuff, again, yay

--- quickbooks_endpoint.rb
+++ quickbooks_endpoint.rb
@@ -1,88 +1,59 @@
-require "sinatra"
-require "endpoint_base"
-
 require File.expand_path(File.dirname(__FILE__) + '/lib/qb_integration')
 
-class QuickbooksEndpoint < EndpointBase::Sinatra::Base 
+class QuickbooksEndpoint < EndpointBase::Sinatra::Base
+
+  Honeybadger.configure do |config|
+    config.api_key = ENV['HONEYBADGER_KEY']
+    config.environment_name = ENV['RACK_ENV']
+  end
+
   post '/add_product' do
-    begin
-      code, summary = QBIntegration::Product.new(@payload, @config).import
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::Product.new(@payload, @config).import
+    result code, summary
   end
 
   post '/update_product' do
-    begin
-      code, summary = QBIntegration::Product.new(@payload, @config).import
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::Product.new(@payload, @config).import
+    result code, summary
   end
 
   post '/add_order' do
-    begin
-      code, summary = QBIntegration::Order.new(@payload, @config).create
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::Order.new(@payload, @config).create
+    result code, summary
   end
 
   post '/update_order' do
-    begin
-      code, summary = QBIntegration::Order.new(@payload, @config).update
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::Order.new(@payload, @config).update
+    result code, summary
   end
 
   post '/cancel_order' do
-    begin
-      code, summary = QBIntegration::Order.new(@payload, @config).cancel
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::Order.new(@payload, @config).cancel
+    result code, summary
   end
 
   post '/add_return' do
-    begin
-      code, summary = QBIntegration::ReturnAuthorization.new(@payload, @config).create
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::ReturnAuthorization.new(@payload, @config).create
+    result code, summary
   end
 
   post '/update_return' do
-    begin
-      code, summary = QBIntegration::ReturnAuthorization.new(@payload, @config).update
-      result code, summary
-    rescue => e
-      result 500, "#{e.message}"
-    end
+    code, summary = QBIntegration::ReturnAuthorization.new(@payload, @config).update
+    result code, summary
   end
 
   post '/get_inventory' do
-    begin
-      stock = QBIntegration::Stock.new(@payload, @config)
+    stock = QBIntegration::Stock.new(@payload, @config)
 
-      if stock.name.present? && stock.item
-        add_object :inventory, { sku: stock.item.name, quantity: stock.item.quantity_on_hand.to_i }
-        result 200
-      elsif stock.items.present?
-        stock.inventories.each { |item| add_object :inventory, item }
-        add_parameter 'quickbooks_poll_stock_timestamp', stock.last_modified_date
-        result 200
-      else
-        result 200
-      end
-    rescue => e
-      result 500, "#{e.message}"
+    if stock.name.present? && stock.item
+      add_object :inventory, { sku: stock.item.name, quantity: stock.item.quantity_on_hand.to_i }
+      result 200
+    elsif stock.items.present?
+      stock.inventories.each { |item| add_object :inventory, item }
+      add_parameter 'quickbooks_poll_stock_timestamp', stock.last_modified_date
+      result 200
+    else
+      result 200
     end
   end
 end
