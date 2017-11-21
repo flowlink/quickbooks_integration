6269071c56ccdaf37c1ee552205a4d7dae18a619
Marc Salo <marc@nurelm.com>
2017-11-21 11:47:46 -0500

    Adds debug 'puts'

--- lib/qb_integration/services/payment_method.rb
+++ lib/qb_integration/services/payment_method.rb
@@ -24,13 +24,16 @@ module QBIntegration
         # NOTE due to bug which might send the mapping as a string. e.g.
         #
         #   "[{\"visa\":\"credit-card\",\"master-card\":\"credit-card\"}]"
+        #   "[{\"PayPal\":\"PayPal\",\"None\":\"Cash\",\"Online Credit Card\":\"Credit Card\"}]"
+        #   "[{\"PayPal\":\"PayPal\",\"Mastercard\":\"Mastercard\",\"Visa\":\"Visa\",\"Discover\":\"Discover\",\"American Express\":\"American Express\",\"None\":\"Cash\",\"Online Credit Card\":\"Credit Card\"}]"
         #
         if config.fetch("quickbooks_payment_method_name").is_a? String
+          puts "About to Parse";
           payment_method_name_mapping = JSON.parse(config.fetch("quickbooks_payment_method_name"))
         else
           payment_method_name_mapping = config.fetch("quickbooks_payment_method_name")
         end
-
+        puts payment_method_name_mapping
         lookup_value!(payment_method_name_mapping.first, augury_name)
       end
 
