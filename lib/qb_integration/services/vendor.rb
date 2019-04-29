678d3e0508d1f9759054aa4a9f82bed8291c69f8
Raymond Sapida <ray.sapida@gmail.com>
2019-04-29 07:59:26 -0700

    Update `purchase_order#build` to search for vendor id before the name

--- lib/qb_integration/services/vendor.rb
+++ lib/qb_integration/services/vendor.rb
@@ -4,6 +4,15 @@ module QBIntegration
       def initialize(config)
         super("Vendor", config)
       end
+
+      def find_by_id(id)
+        util = Quickbooks::Util::QueryBuilder.new
+        clause = util.clause("id", "=", id)
+        vendor = @quickbooks.query("select * from Vendor where #{clause}").entries.first
+        raise "No Vendor '#{name}' defined in service" unless vendor
+        vendor
+      end
+
       def find_by_name(name)
         util = Quickbooks::Util::QueryBuilder.new
         clause = util.clause("CompanyName", "=", name)
