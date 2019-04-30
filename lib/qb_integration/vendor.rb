cd4670b9344921e066025a143aa3ce919cacf554
Raymond Sapida <ray.sapida@gmail.com>
2019-04-30 10:43:12 -0700

    Update Vendor model to parse referenced model fields when returning hash

--- lib/qb_integration/vendor.rb
+++ lib/qb_integration/vendor.rb
@@ -36,13 +36,38 @@ module QBIntegration
     def as_flowlink_hash(vendor)
       {
         id: vendor.id,
+        last_updated_time: vendor.meta_data['last_updated_time'],
         name: vendor.display_name,
-        phone: vendor.primary_phone,
-        email: vendor.primary_email_address,
-        website: vendor.web_site,
-        address: vendor.billing_address,
+        phone: parse_number(vendor),
+        email: parse_email(vendor),
+        website: parse_website(vendor),
+        address: parse_address(vendor),
         currency: vendor.currency_ref['value']
       }
+    end
+
+    def parse_address(vendor)
+      unless vendor.billing_address.nil?
+        {
+          street1: vendor.billing_address.fetch('line1', nil),
+          street2: vendor.billing_address.fetch('line2', nil),
+          city: vendor.billing_address.fetch('city', nil),
+          country: vendor.billing_address.fetch('country', nil),
+          zipcode: vendor.billing_address.fetch('postal_code', nil),
+        }.compact
+      end
+    end
+
+    def parse_number(vendor)
+      vendor.primary_phone.fetch('free_form_number', nil) unless vendor.primary_phone.nil?
+    end
+
+    def parse_email(vendor)
+      vendor.primary_email_address.fetch('address', nil) unless vendor.primary_email_address.nil?
+    end
+
+    def parse_website(vendor)
+      vendor.web_site.fetch('uri', nil) unless vendor.web_site.nil?
     end
   end
 end
