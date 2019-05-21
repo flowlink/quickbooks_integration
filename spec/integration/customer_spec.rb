30982864566cc9c3706a003e5d12b594175d0e03
Raymond Sapida <ray.sapida@gmail.com>
2019-05-21 13:59:34 -0700

    Extract common configurations to a spec_helper file in integration dir

--- spec/integration/customer_spec.rb
+++ spec/integration/customer_spec.rb
@@ -1,19 +1,4 @@
-require 'rack/test'
-require 'rubygems'
-require 'bundler'
-require 'vcr'
-Bundler.require(:default, :test)
-require File.join(File.dirname(__FILE__), '..', '../lib/qb_integration.rb')
-require File.join(File.dirname(__FILE__), '..', '../quickbooks_endpoint')
-
-VCR.configure do |c|
-  c.cassette_library_dir = "spec/vcr"
-  c.hook_into :webmock
-  c.filter_sensitive_data('<REALM>') { ENV.fetch('quickbooks_realm') }
-  c.filter_sensitive_data('<TOKEN>') { ENV.fetch('quickbooks_access_token') }
-  c.filter_sensitive_data('<SECRET>') { ENV.fetch('quickbooks_access_secret') }
-  c.configure_rspec_metadata!
-end
+require_relative 'spec_helper'
 
 describe 'App' do
   let(:headers) {
