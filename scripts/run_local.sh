7d6471f31a8689d937e030392d7a6d5163604ca6
Joaquin <joaquin@nurelm.com>
2021-04-29 17:58:00 -0300

    Changed OS to Alpine. Cleared up Docker.

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml \
-               -f docker-compose.dev.yml \
-               ${@:-up -d}
+docker-compose ${@:-up -d}
--- scripts/run_local_rspec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker-compose -f docker-compose.yml \
-               -f docker-compose.dev.yml \
-               run --rm quickbooks-integration \
-               sh -c "bundle exec rspec ${@}"
